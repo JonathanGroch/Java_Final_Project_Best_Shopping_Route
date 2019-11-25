54ed3d7db30b57eb35d56f6dead0559d9a44799c
offended <modernarcheops@gmail.com>
2019-11-25 17:09:24 -0600

    made sql drivers work somehow??????????????

--- BriefTrek/src/BT_Package/JDBCConnect.java
+++ BriefTrek/src/BT_Package/JDBCConnect.java
@@ -1,20 +1,23 @@
 package BT_Package;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCConnect {
-	JDBCConnect() throws ClassNotFoundException{
+	JDBCConnect() throws ClassNotFoundException, InstantiationException, IllegalAccessException{
 		//Initialize connection to database
 		// Load the JDBC driver
-		Class.forName("com.mysql.cj.jdbc.Driver");
+		Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
 		System.out.println("Driver loaded");
 		
 	}
 	static Connection getConnection() throws SQLException {
 		// Establish a connection
-		String myUrl = "jdbc:mysql://localhost/StoreNavigator?useSSL=false";
+		String myUrl = "jdbc:mysql://localhost:3306/StoreNavigator";
+		DriverManager.getDrivers();
 		Connection connection = DriverManager.getConnection(myUrl, "root", "sesame");
 		System.out.println("Database connected");
 		return connection;
